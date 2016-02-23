b7cd3d22b8d23986a7af12b79f230836ba4fee39
alexstolznut <alexstolznut@hotmail.com>
2016-02-23 02:32:35 -0800

    just need footer

---
+++ www/script.js
@@ -0,0 +1,39 @@
+var nameApp = angular.module('starter', ['ionic', 'ngMessages']);
+ 
+nameApp.config(function($stateProvider, $urlRouterProvider) {
+  
+  $stateProvider
+    .state('auth', {
+      url: '/auth',
+      templateUrl: 'auth.html',
+      controller: 'AuthCtrl'
+    })
+    .state('home', {
+      url: '/home',
+      templateUrl: 'home.html',
+      controller: 'HomeCtrl'
+    });
+  
+  $urlRouterProvider.otherwise("/auth");
+  
+});
+ 
+ 
+nameApp.controller('AuthCtrl', function($scope, $state) {
+   
+  $scope.authorization = {
+    username: '',
+    password : ''   
+  };  
+   
+  $scope.signIn = function(form) {
+    if(form.$valid) {
+      $state.go('home');
+    }
+  };  
+ 
+});
+  
+nameApp.controller('HomeCtrl', function($scope) {
+ 
+});
